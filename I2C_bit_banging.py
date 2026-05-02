813d9c42a1a734324deb7e737a041524cab9ef0d
AgustinNavari <navari.agustin@gmail.com>
2026-05-02 14:15:14 -0300

    random read

--- I2C_bit_banging.py
+++ I2C_bit_banging.py
@@ -9,10 +9,26 @@ sleep_time = 0.00001
 GPIO.setmode(GPIO.BCM) #define como se nombran los pines
 GPIO.setwarnings(False) #para que no grite por setear los pines muchas veces
 
-msg = 0b010100001
+msg_write = 0b10100000
+msg_read  = 0b10100001
 
+wrd_add_1 = 0b00000000
+wrd_add_2 = 0b00000010
+
+'''
 start(DATA,CLK)
-print(send_msg(msg,8,DATA,CLK))
+print(send_msg(msg_read,8,DATA,CLK))
 print(read_byte(DATA,CLK))
 send_ack_bit(DATA,CLK,1)
+stop(DATA,CLK)
+'''
+
+start(DATA,CLK)
+print(send_msg(msg_write, 8,DATA, CLK))
+print(send_msg(wrd_add_1, 8, DATA, CLK))
+print(send_msg(wrd_add_2, 8, DATA, CLK))
+start(DATA,CLK)
+print(send_msg(msg_read,8,DATA,CLK))
+print(read_byte(DATA,CLK))
+send_ack_bit(DATA,CLK,ack=False)
 stop(DATA,CLK)
