5cb703c8a406b0a9cba05f9ff3a2d36bdb2f2b09
AgustinNavari <navari.agustin@gmail.com>
2026-05-02 01:59:04 -0300

    current address read test

--- I2C_bit_banging.py
+++ I2C_bit_banging.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 import time
-from I2C_functions import send_msg, start
+from I2C_functions import send_msg, start, send_ack_bit, stop, read_byte
 
 CLK = 11
 DATA = 10
@@ -9,9 +9,10 @@ sleep_time = 0.00001
 GPIO.setmode(GPIO.BCM) #define como se nombran los pines
 GPIO.setwarnings(False) #para que no grite por setear los pines muchas veces
 
+msg = 0b010100001
 
 start(DATA,CLK)
-
-msg = 0b01010101
-
 print(send_msg(msg,8,DATA,CLK))
+print(read_byte(DATA,CLK))
+send_ack_bit(DATA,CLK,1)
+stop(DATA,CLK)
